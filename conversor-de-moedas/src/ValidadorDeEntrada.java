2ffa55e56d97ea10f5daae43c6a5548da41d8498
wesley <wesley.gomes222@hotmail.com>
2023-05-13 20:34:52 -0300

    subindo o projeto

---
+++ conversor-de-moedas/src/ValidadorDeEntrada.java
@@ -0,0 +1,10 @@
+public class ValidadorDeEntrada {
+	public boolean entradaValida(String entrada) {
+		try {
+			Double.parseDouble(entrada);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+}
